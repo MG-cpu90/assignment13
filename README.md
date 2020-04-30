078074440bd712d8b7aa73f16e6a909ca16d2ced
Massimo Guida <massimo.guidamusic@gmail.com>
2020-04-30 18:41:52 -0400

    update folder structure

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# assignment13
+# Assignment 13 – Node Express Handlebars: "Eat-Da-Burger!" Restaurant App
+
+## User Story
+
+## Description
+
+## Deployed Links
+* GitHub Repository URL: https://github.com/MG-cpu90/assignment13
+* Deployed URL:
---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoked Bacon Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken–Pesto Parmigiano Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Beef Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Fillet Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gluten Free Bun Vegan Burger', true);
